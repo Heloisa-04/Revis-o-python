f10881d502bcc0a70acb7b0b0febec607abd98c7
Heloisa-04 <heloisasenaim16@gmail.com>
2024-08-02 07:42:24 -0300

    Criação do arquuvo de funções do gerenciador de tarefas

--- To do.py
+++ To do.py
@@ -2,7 +2,32 @@
 #Excluir tarefas
 #Editar tarefas
 
+
+import os 
+
 sair = 0 
 
 while sair == 0:
     print("Gerenciador de tarefas - TO DO")
+    print("")
+    print("O que deseja fazer?")
+    print("1- Adicionar uma tarefa")
+    print("2- Editar tarefa")
+    print("3- Excluir tarefa")
+    print("4- sair")
+    escolha = int(input("Insira sua resposta:"))
+
+    if escolha == 1:
+        pass
+
+    elif escolha == 2:
+        pass
+
+    elif escolha == 3:
+        pass
+
+    elif escolha == 4:
+        sair = 1
+        
+
+os.system("pause")
